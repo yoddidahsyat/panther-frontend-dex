07bf4f450cf23236cbdb2861dd91c7f657b4581a
yoddi dahsyat <yoddi.dahsyat@gmail.com>
2021-12-30 18:35:39 +0700

    update home text center

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 // import Question from 'components/QuestionHelper'
 import FullPositionCard from 'components/PositionCard'
 // import { useTokenBalancesWithLoadingIndicator } from 'state/wallet/hooks'
-import { StyledInternalLink, TYPE } from 'components/Shared'
+// import { StyledInternalLink, TYPE } from 'components/Shared'
 import { LightCard } from 'components/Card'
 // import { RowBetween } from 'components/Row'
 import { AutoColumn } from 'components/Column'
@@ -26,16 +26,16 @@ import { AutoColumn } from 'components/Column'
 // import AppBody from '../AppBody'
 
 // import Standard from './Standard'
-const { body: Body } = TYPE
+// const { body: Body } = TYPE
 
 const Home = () => {
 
   return (
     <>
-      <LightCard padding='2rem 5rem'>
+      <LightCard padding='2rem 3rem'>
         <AutoColumn gap='lg' justify='center'>
-          <Text fontSize='30px'>THE LAUNCHPAD PROTOCOL FOR CRYPTO ENTHUSIAST</Text>
-          <Body style={{textAlign: 'center'}} >DigiLaunchpad come to help everyone to create their own tokens, lock LP in few seconds. The purpose of DigiLaunchpad is to reduce fraud from scam projects. Future Launchpad to making safe everyone in crypto world. But remember to still DYOR!</Body>
+          <Text fontSize='2rem' style={{ textAlign: 'center' }}>THE LAUNCHPAD PROTOCOL FOR CRYPTO ENTHUSIAST</Text>
+          <Text style={{textAlign: 'center'}} >DigiLaunchpad come to help everyone to create their own tokens, lock LP in few seconds. The purpose of DigiLaunchpad is to reduce fraud from scam projects. Future Launchpad to making safe everyone in crypto world. But remember to still DYOR!</Text>
           <Button as={Link} to="/token/create">Create Now</Button>
         </AutoColumn>
       </LightCard>
